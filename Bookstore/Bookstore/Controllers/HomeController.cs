e6497be36d74965d9f0343c5a0f853eb117d54e6
d3m1rt45 <d3m1rt45@outlook.com>
2019-09-12 01:05:53 +0100

    Image Displays and Links for Books are Automated
    Rather than having a block of code for each book displayed in the Home/Index, we now have a for loop for each section that is getting the ISBN for the link and ImagePath for the display of image.

--- Bookstore/Bookstore/Controllers/HomeController.cs
+++ Bookstore/Bookstore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bookstore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,13 @@ namespace Bookstore.Controllers
 {
     public class HomeController : Controller
     {
+        private BookstoreContext db = new BookstoreContext();
+
         public ActionResult Index()
         {
-            return View();
+            var bookList = db.Books.ToList();
+
+            return View(bookList);
         }
 
         public ActionResult About()
