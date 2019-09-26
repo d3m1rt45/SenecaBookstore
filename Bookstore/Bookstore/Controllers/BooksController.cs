7f46d6df5d5a1340c2a9f86553d6773e506fa8fd
d3m1rt45 <d3m1rt45@outlook.com>
2019-09-26 01:53:40 +0100

    Unnecessary Files Deleted

--- Bookstore/Bookstore/Controllers/BooksController.cs
+++ Bookstore/Bookstore/Controllers/BooksController.cs
@@ -20,7 +20,7 @@ namespace Bookstore.Controllers
         // GET: Books
         public ActionResult Index(string search, string sortBy)
         {
-            var cardsList = BooksIndexViewModel.CardsList(db.Books); //Make Smaller 'BookCardViewModel' Objects from a List of Cards
+            var cardsList = BooksIndexViewModel.CardsList(db.Books); //Make a List<BooksIndexViewModel> Object Populated from db.Books
 
             //SEARCH
             if (!String.IsNullOrEmpty(search))  //Only Executed if A Search String is Passed
@@ -109,90 +109,6 @@ namespace Bookstore.Controllers
             return View(book);
         }
 
-        // GET: Books/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Book book = db.Books.Find(id);
-            if (book == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(book);
-        }
-
-        // POST: Books/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                Book duplicate = db.Books.Find(book.ISBN);
-
-                Author prevAuthor = duplicate.Author;
-                Genre prevGenre = duplicate.Genre;
-
-
-                db.Books.Remove(duplicate);
-
-                if (prevAuthor.Books.Count() < 1)
-                    db.Authors.Remove(prevAuthor);
-
-                if (prevGenre.Books.Count() < 1)
-                    db.Genres.Remove(prevGenre);
-
-                db.SaveChanges();
-
-                if (book.ImageFile.FileName != null)
-                {
-                    string imageFileName = Path.Combine(Server.MapPath("~/Images"), duplicate.ImagePath);
-                    System.IO.File.Delete(imageFileName);
-
-                    string extension = Path.GetExtension(book.ImageFile.FileName);
-                    imageFileName = book.ISBN + extension;
-                    book.ImagePath = imageFileName;
-                    imageFileName = Path.Combine(Server.MapPath("~/Images"), imageFileName);
-                    book.ImageFile.SaveAs(imageFileName);
-                }
-
-                Author author = db.Authors.Find(book.AuthorName);
-                Genre genre = db.Genres.Find(book.GenreName);
-
-                if (author == null)
-                    author = new Author() { Name = book.AuthorName };
-
-                if (genre == null)
-                    genre = new Genre() { Name = book.GenreName };
-
-                if (author.Books == null)
-                    author.Books = new List<Book>();
-
-                if (genre.Books == null)
-                    genre.Books = new List<Book>();
-
-                if (!author.Books.Contains(book))
-                    author.Books.Add(book);
-
-                if (!genre.Books.Contains(book))
-                    genre.Books.Add(book);
-
-                book.Genre = genre;
-                book.Author = author;
-
-                db.Books.Add(book);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(book);
-        }
-
         public ActionResult ByISBN(string role)
         {
             Book book = db.Books.Find(role);
@@ -219,22 +135,6 @@ namespace Bookstore.Controllers
             return View(byGenreInstance);
         }
 
-        //SEARCH ACTION
-        public ActionResult Search(string keyword, string author, string genre)
-        {
-            var result = SearchViewModel.SearchTitles(author, genre, keyword);
-            
-            //ANY FOUND?
-            if (result.Books.Any()) //IF YES:
-            {
-                return View("Found", result); //RETURN 'Found' VIEW
-            }
-            else //IF NO:
-            {
-                return View("NotFound", result);  //RETURN 'NotFound' VIEW
-            }
-        }
-
         public ActionResult ByAuthor(string authorName, string order, string search)
         {
             var thisAuthorBookCards = BooksIndexViewModel.CardsList(db.Authors.Find(authorName).Books);
@@ -253,6 +153,22 @@ namespace Bookstore.Controllers
             var byAuthorInstance = new ByAuthorViewModel { BookCards = thisAuthorBookCards, Author = authorName };
 
             return View(byAuthorInstance);
+        }
+
+        //SEARCH ACTION
+        public ActionResult Search(string keyword, string author, string genre)
+        {
+            var result = SearchViewModel.SearchTitles(author, genre, keyword);
+
+            //ANY FOUND?
+            if (result.Books.Any()) //IF YES:
+            {
+                return View("Found", result); //RETURN 'Found' VIEW
+            }
+            else //IF NO:
+            {
+                return View("NotFound", result);  //RETURN 'NotFound' VIEW
+            }
         }
 
         // GET: Books/Delete/5
